517d866ee04be0efb3202e2344c2c4a8eb0d291f
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2021-04-24 08:48:12 +0600

    Alhumdulillah Done

--- app.js
+++ app.js
@@ -73,4 +73,21 @@ function showDetails(devices){
 }
 document.getElementById('year-date').innerHTML = new Date().getFullYear();
 
+var scrollUpButton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollUpButton.style.display = "block";
+  } else {
+    scrollUpButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
