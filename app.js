9d893fd77b3989d7b4eeba9a2efaa4b91bdf08ff
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2021-04-24 08:02:31 +0600

    Alhumdulillah Done

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const main = document.querySelector('#main');
+// AOS.init();
 // console.log(main)
 function getData(){
-    
+    // console.log("false")
+    spinToggler(false);
     fetch('https://my-all-works-server.herokuapp.com/')
     .then(res => res.json())
     .then(data => {
@@ -10,19 +12,33 @@ function getData(){
         showDetails(data);
     })
 }
+
+const spinToggler = (show) =>{
+    // console.log(show);
+    if(show){
+        // console.log(document.getElementById('spinner-loader').classList)
+        document.getElementById('spinner-loader').className = "d-none my-5 spinner-border text-warning";
+    }
+    else{
+        // console.log(document.getElementById('spinner-loader').classList)
+        document.getElementById('spinner-loader').className = "d-block my-5 spinner-border text-warning";
+    }
+}
+
 getData();
+
 
 function showData(data){
     // console.log(data);
     const row = document.createElement('div');
     row.className = "row py-2"
-
+    spinToggler(true);
     data.forEach(d => {
         const div = document.createElement('div')
         div.className = 'col-lg-4 col-md-6 col-xs-6 my-2';
         div.innerHTML = `<a  href="#${d.name}" class='card hover bg-white text-center p-2 bg-light rounded text-decoration-none text-dark shadow'>
             <h4>${d.name}</h4>
-            <img style="height: 200px" class="img-fluid w-100 mx-auto" src="${d.img}" alt=""/>
+            <img  style="height: 200px" class="img-fluid w-100 mx-auto" src="${d.img}" alt=""/>
         </a>`;
         row.appendChild(div);
     });
@@ -46,7 +62,9 @@ function showDetails(devices){
         div.innerHTML = `
             <img style="height: 350px" class="img-fluid device-img mx-auto" src="${device.img}" alt=""/>
             <h3 class="text-success my-3">${device.name}</h3>
-            <p>Invention: ${device.inventor}</p>
+            <div data-aos="fade-up" data-aos-delay="100" data-aos-duration="2000">
+                <h3>${device.inventor}</h3>
+            </div>
             <p style='height: 160px' class="text-warning scroll-off"><small>${device.des}</small></p>
         `;
         article.appendChild(div);
@@ -54,3 +72,5 @@ function showDetails(devices){
     main.appendChild(article);
 }
 document.getElementById('year-date').innerHTML = new Date().getFullYear();
+
+
